b3fdf13e89df3dab8981133acefdf1369d0a7de4
Gurpreet <gurpreetbarpagga@yahoo.com>
2022-07-12 02:17:35 -0400

    New delete button and event listener for it. Local data storage functionality

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(storedLeads)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+//console.log(leadsFromLocalStorage)
+
+//Stores data over refresh
+if(leadsFromLocalStorage)
+{
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+//Delete btn to clear all data from localstorage, array and DOM
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+//Btn to save data to myLeads array in local storage
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
@@ -14,6 +32,7 @@ inputBtn.addEventListener("click", function(){
     console.log(localStorage.getItem("myLeads"))
 })
 
+//Print out the input into a html list
 function renderLeads(){
     let listItems = ""
     for(let i = 0; i < myLeads.length; i++){
