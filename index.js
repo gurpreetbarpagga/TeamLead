3cf99c859256c1d638e2516c9ae56079d1ec0efc
Gurpreet <gurpreetbarpagga@yahoo.com>
2022-07-12 18:36:41 -0400

    Added event listener for save tab button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
+const savetabBtn = document.getElementById("savetab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 //console.log(leadsFromLocalStorage)
@@ -13,14 +13,38 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsFromLocalStorage)
 {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
+}
+
+//Button to save the current opened tab URL to the list
+savetabBtn.addEventListener("click", function(){
+    //Chrome extension API to get URL from current window
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myleads))
+    render(myLeads)
+    })
+})
+
+//Print out the input into a html list using string literal
+function render(leads){
+    let listItems = ""
+    for(let i = 0; i < leads.length; i++){
+        listItems += `
+        <li>
+        <a href='${leads[i]}' target='_blank'> 
+        ${leads[i]}
+        </a> 
+        </li>`
+    }
+    ulEl.innerHTML = listItems
 }
 
 //Delete btn to clear all data from localstorage, array and DOM
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 //Btn to save data to myLeads array in local storage
@@ -28,20 +52,5 @@ inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-    console.log(localStorage.getItem("myLeads"))
+    render(myLeads)
 })
-
-//Print out the input into a html list
-function renderLeads(){
-    let listItems = ""
-    for(let i = 0; i < myLeads.length; i++){
-        listItems += `
-        <li>
-        <a href='${myLeads[i]}' target='_blank'> 
-        ${myLeads[i]}
-        </a> 
-        </li>`
-    }
-    ulEl.innerHTML = listItems
-}
